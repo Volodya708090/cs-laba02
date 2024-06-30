69fc1c59236ea6150e2b29fbfd29154749726117
Bob <bob@example.com>
2024-06-30 21:12:58 +0300

    Добавлен вывод минимума из чисел

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ int main()
     cout << "A + B = " << a + b << '\n'
          << "A - B = " << a - b << '\n'
          << "A * B = " << a * b << '\n'
-         << "A / B = " << a / b << '\n';
+         << "A / B = " << a / b << '\n'
+         << "min(A, B) = " << min(a, b) << '\n';
     return 0;
 }
